562ef58160bed59d3fa34dc8822da6f6f761166a
renaldyhidayatt <renaldyhidayatt@gmail.com>
2022-11-01 08:43:00 +0700

    Adding

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
+
+        obj.setBounds(10, 10, 905, 700);
+        obj.setBackground(Color.DARK_GRAY);
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+}
